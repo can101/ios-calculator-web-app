bd3a47b593c11f9bc23c683fbd01553b7f54c9e4
can101 <can896373@gmail.com>
2023-01-04 23:41:47 +0300

    style: folder structure

---
+++ src/app/App.tsx
@@ -0,0 +1,15 @@
+import {useState} from 'react'
+import reactLogo from '../assets/react.svg'
+import './style.css'
+
+function App() {
+    const [count, setCount] = useState(0)
+
+    return (
+        <div className="App">
+            hello
+        </div>
+    )
+}
+
+export default App
